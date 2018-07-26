63d4447533a9acaeb2e905329154ee3e9e4ad53d
Jeff Hatcher <wjeffhatcher@gmail.com>
2018-07-26 10:19:09 -0400

    update: remove unused files

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
-//Import a library to help create a component
 import React from 'react';
 import { AppRegistry, View } from 'react-native';
 import Header from './src/components/Header';
 import AlbumList from './src/components/AlbumList';
 
-//Create a component
 const App = () => {
     return (
         <View style={{flex:1}}>
@@ -14,5 +12,4 @@ const App = () => {
     );
 };
 
-//Render it to the device
 AppRegistry.registerComponent('albums', () => App);
